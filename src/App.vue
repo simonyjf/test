8a796c82368c117d773a56b35a7471419384fd0c
yjf <872984131@qq.com>
2020-06-08 15:29:42 +0800

    hello

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
   <div id="app">
     <div id="nav">
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
+      <router-link to="/about">你好</router-link>
     </div>
     <router-view/>
   </div>
